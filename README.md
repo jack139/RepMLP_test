75fa474311c6626acb8e6a72649b5b893e760369
gt <jack139@gmail.com>
2021-07-26 11:12:04 +0800

    模型转换deploy及测试

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 ## RepMLP TEST
 
-使用pre-trained模型
+### 使用pre-trained模型
+
+使用训练模型
 ```
 python3 test.py data/ train ../face_model/RepMLP/RepMLP-Res50-light-224_train.pth -a RepMLP-Res50-light-224
 ```
@@ -15,7 +17,21 @@ python3 convert.py ../face_model/RepMLP/RepMLP-Res50-light-224_train.pth RepMLP-
 python3 test.py data/ deploy data/RepMLP-Res50-light-224_deploy.pth -a RepMLP-Res50-light-224
 ```
 
-训练测试
+
+
+### 重新训练
+
+训练
 ```
 python3 train.py
+```
+
+转换
+```
+python3 convert.py data/checkpoint.ckpt.b160_e14_0.9395 data/checkpoint.ckpt.b160_e14_0.9395.deploy -a face-light-96
+```
+
+测试
+```
+python3 test2.py data/ deploy data/checkpoint.ckpt.b160_e14_0.9395.deploy -a face-light-96 -r 96
 ```
--- convert.py
+++ convert.py
@@ -15,21 +15,41 @@ parser.add_argument('-a', '--arch', metavar='ARCH', default='RepMLP-Res50-light-
 
 def convert():
     args = parser.parse_args()
-    if args.arch == 'RepMLP-Res50-light-224':
-        train_model = create_RepMLPRes50_Light_224(deploy=False)
-    else:
-        raise ValueError('TODO')
 
     if os.path.isfile(args.load):
         print("=> loading checkpoint '{}'".format(args.load))
-        checkpoint = torch.load(args.load)
-        if 'state_dict' in checkpoint:
-            checkpoint = checkpoint['state_dict']
-        elif 'model' in checkpoint:
-            checkpoint = checkpoint['model']
-        ckpt = {k.replace('module.', ''): v for k, v in checkpoint.items()}  # strip the names
-        print(ckpt.keys())
-        train_model.load_state_dict(ckpt)
+        checkpoint = torch.load(args.load, map_location=torch.device('cpu'))
+
+        # 初始model
+        if args.arch == 'RepMLP-Res50-light-224':
+            train_model = create_RepMLPRes50_Light_224(deploy=False)
+
+            if 'state_dict' in checkpoint:
+                checkpoint = checkpoint['state_dict']
+            elif 'model' in checkpoint:
+                checkpoint = checkpoint['model']
+            ckpt = {k.replace('module.', ''): v for k, v in checkpoint.items()}  # strip the names
+            print(ckpt.keys())
+            train_model.load_state_dict(ckpt)
+
+        elif args.arch == 'face-light-96':
+            last_label = checkpoint['label_dict']
+
+            train_model = RepMLPResNet(num_blocks=[3,4,6,3], num_classes=len(last_label), 
+                block_type='light', img_H=96, img_W=96,
+                h=6, w=6, reparam_conv_k=(1,3,5), fc1_fc2_reduction=1, fc3_groups=4,
+                deploy=False)
+
+            train_model.load_state_dict(checkpoint['model_state_dict'])
+            #optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+            total_epochs = checkpoint['epoch']
+            last_loss = checkpoint['loss']            
+            print(f"Loaded {args.load}: epochs= {total_epochs}, loss= {last_loss:.6f}, num_classes= {len(last_label)}")
+
+        else:
+            raise ValueError('TODO')
+
+        
     else:
         print("=> no checkpoint found at '{}'".format(args.load))
 
